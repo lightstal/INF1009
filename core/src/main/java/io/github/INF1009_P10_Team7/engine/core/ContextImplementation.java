6de18c309cd05ecef917158290109fd353299249
lightstal <50443758+lightstal@users.noreply.github.com>
2026-02-09 13:56:09 +0800

    Add EntityManager to ContextImplementation

--- core/src/main/java/io/github/INF1009_P10_Team7/engine/core/ContextImplementation.java
+++ core/src/main/java/io/github/INF1009_P10_Team7/engine/core/ContextImplementation.java
@@ -1,16 +1,19 @@
 package io.github.INF1009_P10_Team7.engine.core;
 
+import io.github.INF1009_P10_Team7.engine.entity.EntityManager;
 import io.github.INF1009_P10_Team7.engine.events.EventBus;
 import io.github.INF1009_P10_Team7.engine.inputoutput.InputController;
 
 public class ContextImplementation implements GameContext {
     private final EventBus eventBus;
     private final InputController inputController;
+    private final EntityManager entityManager;
 
     // Constructor accepts the Interface
-    public ContextImplementation(EventBus eventBus, InputController inputController) {
+    public ContextImplementation(EventBus eventBus, InputController inputController, EntityManager entityManager) {
         this.eventBus = eventBus;
         this.inputController = inputController;
+        this.entityManager = entityManager;
     }
 
     @Override
@@ -21,5 +24,10 @@ public class ContextImplementation implements GameContext {
     @Override
     public InputController getInputController() {
         return inputController;
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return entityManager;
     }
 }
